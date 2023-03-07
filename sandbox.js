df5a0765d54d1739ae2c5ecd7dcd8d5ab2bd65f2
Vishal KR <vishal22.raji@gmail.com>
2023-03-07 18:34:17 +0530

    first commit

---
+++ sandbox.js
@@ -0,0 +1,125 @@
+//let result=['shaun','ry','brooo'];
+
+//let ninjas=result.join(' ');
+// const speak = function(name= 'luigi' , time='time is 6 am'){
+
+//     console.log(`good day! ${name} ${time}`);
+// }
+
+
+// greet();
+// greet();
+// speak('Vishal','time is 10 am');
+// speak();
+// function greet(){
+//     console.log("HEY");
+// }
+
+// let rad=10;
+// const pi=3.14;
+// console.log(rad,pi);
+
+// //math operators +,-,*,/,**,%
+// //operations: B I D M A S
+// //I=**
+// const title="hello sir";
+// const dd=22;
+// const name="Vishal";
+// let result=`${title} My name is ${name} dob is ${dd}`;
+// console.log(result);
+
+
+// let age=25;
+// let year =2019;
+// console.log(age,year);
+// age=30;
+// console.log(age,year);
+// const points=100;
+// console.log(points);
+// var score=75;
+// let firstname="Vishal";
+// let lastname="KR";
+// let fullname=firstname+' '+lastname;
+// console.log(fullname);
+// console.log(fullname.length);
+// console.log(fullname.toUpperCase());
+// console.log(fullname.toLowerCase());
+// let result=fullname.toLowerCase();
+// console.log(result);
+// let email="mario@gmail.com";
+// let index=email.indexOf("@");
+// console.log(index);
+// let bro=email.lastIndexOf("r");
+// console.log(bro);
+// let kay=email.slice(1,6);
+// console.log(kay);
+// let may=email.substr(1,6);
+// console.log(may);
+// let na=email.replace("m","w");
+// console.log(na);
+// function area(radius){
+//     let ar=3.14*radius**2;
+//     return ar;
+// }
+//arrow func
+// const area=(radius)=>{
+//     let ar=3.14*radius**2;
+//     return ar;
+// }
+// let a=10;
+//  console.log(area(a))   ;
+
+// const ul = document.querySelector('.people');
+// let html=``;
+// let people=['hello','my','name','is' ,'you'];
+// people.forEach((person)=>{
+// html+=`<li style="color:red">${person}</li>`
+// //console.log(`${index} -- ${person}`);
+// });
+// console.log(html);
+// ul.innerHTML=html;
+// 
+
+// console.log(user);
+// console.log(user.name);
+// user.age=35;
+// console.log(user.age);
+// console.log(user['location']);
+// console.log(typeof(user));
+//let user={
+    //     name:'crystal',
+    //     age:30,
+    //     email:'crystal@thenetninja.co.uk',
+    //     location:'berlin',
+    //     blogs:['Why mac and cheese rules','best days'],
+    //     login:function(){
+    //         console.log('the user logged in');
+    //     },
+    //     logout:function(){
+    //         console.log('the user logged out');
+    //     },
+    //     logBlogs: function(){
+    //         console.log('user has written the following blogs:');
+    //     this.blogs.forEach(blog=>{
+    //         console.log(blog);
+    //     })
+    //     }
+    
+    // };
+    // user.login();
+    // user.logout();
+    // const name ='mario';
+    // //name.toUppercase();
+    // user.logBlogs();
+    // console.log(this);
+//Math object
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.E);
+// const a=10.56;
+// console.log(Math.round(a));
+// console.log(Math.floor(a));
+// console.log(Math.trunc(a));
+// console.log(Math.ceil(a));
+// const random=Math.random();
+// console.log(Math.round(random*100));
